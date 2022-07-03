07c68fb07563fb30ba3df59a8cd70790d15be2a7
Sourcery AI <>
2022-07-03 13:08:56 +0000

    'Refactored by Sourcery'

--- sharer_pw.py
+++ sharer_pw.py
@@ -19,22 +19,19 @@ If you use logout from website, cookies will become invalid.
 
 def parse_info(res):
     f = re.findall(">(.*?)<\/td>", res.text)
-    info_parsed = {}
-    for i in range(0, len(f), 3):
-        info_parsed[f[i].lower().replace(' ', '_')] = f[i+2]
-    return info_parsed
+    return {f[i].lower().replace(' ', '_'): f[i+2] for i in range(0, len(f), 3)}
 
 def sharer_pw_dl(url, forced_login=False):
     client = requests.Session()
-    
+
     client.cookies.update({
         "XSRF-TOKEN": XSRF_TOKEN,
         "laravel_session": laravel_session
     })
-    
+
     res = client.get(url)
     token = re.findall("_token\s=\s'(.*?)'", res.text, re.DOTALL)[0]
-    
+
     ddl_btn = etree.HTML(res.content).xpath("//button[@id='btndirect']")
 
     info_parsed = parse_info(res)
@@ -42,34 +39,34 @@ def sharer_pw_dl(url, forced_login=False):
     info_parsed['src_url'] = url
     info_parsed['link_type'] = 'login' # direct/login
     info_parsed['forced_login'] = forced_login
-    
+
     headers = {
         'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
         'x-requested-with': 'XMLHttpRequest'
     }
-    
+
     data = {
         '_token': token
     }
-    
+
     if len(ddl_btn):
         info_parsed['link_type'] = 'direct'
     if not forced_login:
         data['nl'] = 1
-    
+
     try: 
-        res = client.post(url+'/dl', headers=headers, data=data).json()
+        res = client.post(f'{url}/dl', headers=headers, data=data).json()
     except:
         return info_parsed
-    
+
     if 'url' in res and res['url']:
         info_parsed['error'] = False
         info_parsed['gdrive_link'] = res['url']
-    
-    if len(ddl_btn) and not forced_login and not 'url' in info_parsed:
+
+    if len(ddl_btn) and not forced_login and 'url' not in info_parsed:
         # retry download via login
         return sharer_pw_dl(url, forced_login=True)
-    
+
     return info_parsed
 
 # ===================================================================
